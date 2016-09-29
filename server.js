9d85012f39616dae4dffecc6af1b4c434ad8c378
Thomascn <thomascnm@gmail.com>
2016-09-29 18:08:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -130,14 +130,14 @@ function createTemplate (data)
                         ${content}
                         
                     </div>
-                </div>    
+                   
                
                 
                 
            
         </body>
         
-        
+       
     </html>
     
     `;
@@ -171,3 +171,5 @@ var port = 8080; // Use 8080 for local development because you might already hav
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+
+
