98e3ea612080354f075748c1df8f4169a7c47532
Thomascn <thomascnm@gmail.com>
2016-09-30 18:56:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,10 +2,11 @@ console.log('Loaded!');
 var img = document.getElementById('madi');
 function moveright(){
     var marginleft=0;
-    marginleft=marginleft=10;
+    marginleft=marginleft+10;
     img.style.marginleft = marginleft + 'px';
     }
     
    img.onclick = function() {
+       
        var interval = setInterval (moveright, 50);
    } ;
