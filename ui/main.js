c9d4c2897721fc51ac463241c46f46f38088a888
Thomascn <thomascnm@gmail.com>
2016-09-30 18:47:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 console.log('Loaded!');
+var img = document.getElementById('madi');
+function moveright(){
+    var marginleft=0;
+    marginleft=marginleft=10;
+    img.style.marginleft = marginleft + 'px';
+    
+    
+}
