b07bf261c093862ecab4c9f1a48a4d4b709d666c
Thomascn <thomascnm@gmail.com>
2016-09-30 18:28:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -158,6 +158,12 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+
+
 app.get('/:articleName', function (req, res) {
 var articleName = req.params.articleName;    
     
