fbb44d953b62a9e1d311cd2bfe41f3d5ae4b91c9
Thomascn <thomascnm@gmail.com>
2016-09-30 20:31:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text') ;
-element.innerHTML = 'New Value';
+element.innerHTML = '';
 
 var img = document.getElementById('madi');
 
