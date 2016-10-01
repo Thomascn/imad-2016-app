941f60377aa05be2201dcb5d2580692d149dd9ea
Thomascn <thomascnm@gmail.com>
2016-10-01 13:19:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -145,6 +145,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+counter = counter +1;
+res.send (counter.toString());
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -159,11 +165,6 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-var counter = 0;
-app.get('/counter', function (req, res) {
-counter = counter +1;
-res.send (counter.toString());
-});
 
 
 app.get('/:articleName', function (req, res) {
