18f1cc0c2353725cd201025e43e1b1f2ba73a847
Thomascn <thomascnm@gmail.com>
2016-10-01 13:46:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -140,16 +140,17 @@ function createTemplate (data)
     return htmlTemplate;
 }
 
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
 var counter = 0;
 app.get('/counter', function (req, res) {
 counter = counter +1;
 res.send (counter.toString());
 });
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+
 
 
 app.get('/ui/style.css', function (req, res) {
@@ -164,8 +165,6 @@ app.get('/ui/madi.png', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-
-
 
 app.get('/:articleName', function (req, res) {
 var articleName = req.params.articleName;    
