06d6d4487cd6acce7fdb4b836714869afe70492a
Thomascn <thomascnm@gmail.com>
2016-10-01 15:01:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,7 +19,7 @@ button.onclick = function(){
           }
       }
       
-      request.open ('GET', 'http://thomascn.imad.hasura-app.io/counter', true);
+      request.open('GET', 'http://thomascn.imad.hasura-app.io/counter', true);
       request.send(null);
       
       
