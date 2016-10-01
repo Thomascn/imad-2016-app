f15c603d2137ee4c8064dc6d056539fb236dcccd
Thomascn <thomascnm@gmail.com>
2016-10-01 14:43:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,26 @@
 //Counter code
 var button = document.getElementById ('counter');
 
-var counter = 0;
 button.onclick = function(){
   
   
+  var request = new XMLhttpRequest();
+  request.onreadystatechange = function(){
+      
+      if (request.readyState === XMLhttprequest.DONE){
+          
+          if(request.status ===200){
+              
+             var counter = request.responseText;
+              var span = document.getElementById ('count');
+                span.innerHTML = counter.toString();
   
+              
+          }
+      }
+  };
   
-  counter = counter +1;
-  var span = document.getElementById ('count');
-  span.innerHTML = counter.toString();
-  
+
+ 
     
 };
