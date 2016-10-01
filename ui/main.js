1d5b590a9710476bc26239d4f432380f72ef43be
Thomascn <thomascnm@gmail.com>
2016-10-01 11:41:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
-var element=document.getElementById('main-text') ;
-element.innerHTML = '';
+ // var element=document.getElementById('main-text') ;
+ // element.innerHTML = '';
 
 var img = document.getElementById('madi');
 
