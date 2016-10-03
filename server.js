ebd158555c2c932e4daf91cc3b9edc6adb2b679b
Thomascn <thomascnm@gmail.com>
2016-10-03 09:55:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -167,6 +167,10 @@ app.get('/ui/main.js', function (req, res) {
 
 
 
+//app.get('/:articleName', function (req, res) {
+//var articleName = req.params.articleName;   
+//res.send(createTemplate (articles [articleName]) );   
+//});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
