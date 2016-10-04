1425f8978dbb5cf297827ca4e9631971ec83592f
Thomascn <thomascnm@gmail.com>
2016-10-04 18:54:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
 var app = express();
 app.use(morgan('combined'));
 
@@ -15,24 +14,19 @@ ArticleOne:
     date: "Sep 28, 2016",
     content: 
                     `<p>
-                        
                         This is the first article content.This is the first article content.This is the first article content.This is the first article content.This is the first article content.This is the first article content.This is the first article content.This is the first article content.This is the first article content.This is the first article content. 
                         
                     </p>
                     
                      <p>
-                        
                         This is the first article content.This is the first article content.This is the first article content.This is the first article content.This is the first article content.This is the first article content.This is the first article content.This is the first article content.This is the first article content.This is the first article content. 
                         
                     </p>
                     
                     <p>
-                        
                         This is the first article content.This is the first article content.This is the first article content.This is the first article content.This is the first article content.This is the first article content.This is the first article content.This is the first article content.This is the first article content.This is the first article content. 
                         
                     </p>`
-                    
-    
     
 },
 
@@ -86,7 +80,6 @@ ArticleThree:
 
 };
 
-
 function createTemplate (data) 
 { 
     var title = data.title;
@@ -105,9 +98,7 @@ function createTemplate (data)
           <meta name="viewport" content ="width=device-width, initial-scale=1"/>
            <link href="/ui/style.css" rel="stylesheet" />
         
-          
       </head>  
-        
         <body>
             <div class="container">
             <div>
@@ -140,7 +131,6 @@ function createTemplate (data)
     return htmlTemplate;
 }
 
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -151,7 +141,6 @@ counter = counter +1;
 res.send (counter.toString());
 });
 
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -160,22 +149,16 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-
-
 
 //app.get('/:articleName', function (req, res) {
 //var articleName = req.params.articleName;   
 //res.send(createTemplate (articles [articleName]) );   
 //});
 
-
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
-
-
