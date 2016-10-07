9263e7a7234ebb7c7a11bee6078635bb31d678eb
Thomascn <thomascnm@gmail.com>
2016-10-07 12:05:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,7 +136,7 @@ app.get('/', function (req, res) {
 });
 
 var comments = [];
-app.get('/comments/:comment', function (req, res) {
+app.get('/comments', function (req, res) {
   var comment= req.query.comment;
   comments.push (comment);
   res.send (JSON.stringify (comments));
