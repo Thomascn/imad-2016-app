3c80c72856d3ef302e76cdbfec7a9d6f00b6f71f
Thomascn <thomascnm@gmail.com>
2016-10-07 11:56:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -158,7 +158,19 @@ var articleName = req.params.articleName;
 res.send(createTemplate (articles [articleName]) );   
 });
 
+var comments = [];
+app.get('/comments/:comment', function (req, res) {
+  var comment= re.params.name;
+  comments.push (comment);
+  res.send (JSON.stringify (comments));
+  
+    
+    
+});
+
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+
