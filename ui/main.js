8f0f93e0ed444ba4d209fe0df69eefbef788f4fe
Thomascn <thomascnm@gmail.com>
2016-10-07 12:17:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,14 +32,35 @@ var comment = commentinput.value;
 var submit = document.getElementById('Submit');
 submit.onclick = function(){
     
-    var comments = ["",""];
-    var list = '';
-    for (var i=0; i<comments.length; i++) {
-        list += '<li>' + comments[i] + '</li>';
+    var request = new XMLHttpRequest ();
+   request.onreadystatechange = function () {
+      
+
+      if (request.readyState === XMLHttpRequest.DONE) {
+          
+          if(request.status ===200) {
+           
+             var comments = request.responseText;
+             comments = JSON.parse(comments);  
+
+                  var list = '';
+                      for (var i=0; i<comments.length; i++) {
+                         list += '<li>' + comments[i] + '</li>';
         
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+                            }
+                        var ul = document.getElementById('namelist');
+                        ul.innerHTML = list;
+          }
+      }
+      
+  };
+      request.open('GET', 'http://thomascn.imad.hasura-app.io/comments?comment=' + comment, true);
+ 
+      request.send(null);
+     
+    
+};    
     
     
-}
+    
+   
